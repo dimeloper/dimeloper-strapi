5c0f5c1ab85b9194c2ac8fe700fbc0dd9552ccbe
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2023-04-02 21:24:12 +0000

    Include github webhook configuration backup.

---
+++ github-webhook-backup/webhook.js
@@ -0,0 +1,34 @@
+var secret = 'c0Q9w1G8d31tbQN4FV7hwg=='; // Your secret key from Settings in GitHub
+var repo = '~/dimeloper-strapi/'; // path to the root of your Strapi project on server
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+const PM2_CMD = 'cd ~ && pm2 startOrRestart ecosystem.config.js';
+
+http
+.createServer(function(req, res) {
+  req.on('data', function(chunk) {
+    let sig =
+      'sha1=' +
+      crypto
+      .createHmac('sha1', secret)
+      .update(chunk.toString())
+      .digest('hex');
+
+    if (req.headers['x-hub-signature'] == sig) {
+      exec(`cd ${repo} && git pull && ${PM2_CMD}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+    }
+  });
+
+  res.end();
+})
+.listen(8080);
